ce5e78236c1437f4158814fc42e299f47453a03a
Habteab <Habteab@habteab>
2024-01-09 14:29:51 -0500

    added the name()method

--- src/main/java/MaveanDemo1/DemoClass1.java
+++ src/main/java/MaveanDemo1/DemoClass1.java
@@ -2,8 +2,13 @@ package MaveanDemo1;
 
 public class DemoClass1 {
 
-public static void main(String[] args) {
-	
-	System.out.println("this is tutoring practice");
-}
+	public static void main(String[] args) {
+
+		System.out.println("this is tutoring practice");
+	}
+
+// practice the pull request 
+	public void name() {
+		System.out.println("my name is Mebrat");
+	}
 }
